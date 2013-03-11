4a13c85ac240417a14d7026b35756fa7380ba499
Dan Steinicke <dansteinicke@gmail.com>
2013-03-10 19:07:12 -0700

    Finished basic 'hello world sever -- stil needs cleanup

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -9,9 +9,21 @@ exports.tearDown = function(done){
   });
 };
 
-exports.testRespondsToGetRequests = function(test) {
+exports.test_serverRetunshelloWorld = function(test) {
   server.start();
-  http.get("http://localhost:8080", function(response){
-    test.done();
+  var request = http.get("http://localhost:8080");
+  request.on("response", function(response){
+    var receivedData = false;
+    response.setEncoding("utf8");
+
+    test.equals(200, response.statusCode, "status code");
+    response.on("data", function(chunk){
+      receivedData = true;
+      test.equals("Hello World", chunk, "response text");
+    });
+    response.on("end", function(){
+      test.ok(receivedData, "should have received responsedata");
+      test.done();
+    });
   });
 };
