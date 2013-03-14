40295cf18ff596bac318d0e3fcedce430e7340e1
dan <dan@generalthings.com>
2013-03-14 07:48:02 -0700

    Revise node version checking in jakefile

--- jakefile.js
+++ jakefile.js
@@ -1,6 +1,8 @@
 /*global desc, task, jake, fail, complete */
 (function(){
   "use strict";
+  var NODE_VERSION = "v0.8.6";
+
   console.log("_________________________________________________\n\n\n");
 
   var red, green, reset;
@@ -30,7 +32,7 @@
   });
 
   desc("Test everything");
-  task("test", ["node"], function(){
+  task("test", ["nodeVersion"], function(){
     var reporter = require("nodeunit").reporters['default'];
     reporter.run(['src/server/_server_test.js'], null, function(failures){
       if(failures){
@@ -55,50 +57,74 @@
     console.log("5. 'git checkout master'");
   });
 
-  //desc("Ensure correct version of Node is present")'
+  //	desc("Ensure correct version of Node is present. Use 'strict=true' to require exact match");
+  task("nodeVersion", [], function() {
+    function failWithQualifier(qualifier) {
+      fail("Incorrect node version. Expected " + qualifier +
+           " [" + expectedString + "], but was [" + actualString + "].");
+    }
 
-  task("node", [], function(){
-    var DESIRED_NODE_VERSION = 8.4;
+    var expectedString = NODE_VERSION;
+    var actualString = process.version;
+    var expected = parseNodeVersion("expected Node version", expectedString);
+    var actual = parseNodeVersion("Node version", actualString);
 
-    sh("node --version", function(stdout){
-      if(parseFloat(stdout.slice(3)) < DESIRED_NODE_VERSION) {
-        fail("Incorrect node version.  Expected >= 0." + DESIRED_NODE_VERSION);
-        complete();
+    if (process.env.strict) {
+      if (actual[0] !== expected[0] || actual[1] !== expected[1] || actual[2] !== expected[2]) {
+        failWithQualifier("exactly");
       }
-    });
-  }, {async: true });
+    }
+    else {
+      if (actual[0] < expected[0]) failWithQualifier("at least");
+      if (actual[0] === expected[0] && actual[1] < expected[1]) failWithQualifier("at least");
+      if (actual[0] === expected[0] && actual[1] === expected[1] && actual[2] < expected[2]) failWithQualifier("at least");
+    }
 
-  function sh(command, callback){
+  });
+
+  function parseNodeVersion(description, versionString) {
+    var versionMatcher = /^v(\d+)\.(\d+)\.(\d+)$/;    // v[major].[minor].[bugfix]
+    var versionInfo = versionString.match(versionMatcher);
+    if (versionInfo === null) fail("Could not parse " + description + " (was '" + versionString + "')");
+
+    var major = parseInt(versionInfo[1], 10);
+    var minor = parseInt(versionInfo[2], 10);
+    var bugfix = parseInt(versionInfo[3], 10);
+    return [major, minor, bugfix];
+  }
+
+  function sh(command, callback) {
     console.log("> " + command);
+
     var stdout = "";
-    var process = jake.createExec(command, { printStdout: true, printStderr: true });
-    process.on("stdout", function(chunk){
+    var process = jake.createExec(command, {printStdout:true, printStderr: true});
+    process.on("stdout", function(chunk) {
       stdout += chunk;
     });
-    process.on("cmdEnd", function(){
+    process.on("cmdEnd", function() {
       console.log();
       callback(stdout);
     });
     process.run();
   }
 
-  function nodeLintOptions(){
+  function nodeLintOptions() {
     return {
-      bitwise: true,
-      curly: false,
-      eqeqeq: true,
-      forin: true,
-      immed: true,
-      latedef: true,
-      newcap: true,
-      noarg: true,
-      noempty: true,
-      nonew: true,
-      regexp: true,
-      undef: true,
-      strict: true,
-      trailing: true,
-      node: true
+      bitwise:true,
+      curly:false,
+      eqeqeq:true,
+      forin:true,
+      immed:true,
+      latedef:true,
+      newcap:true,
+      noarg:true,
+      noempty:true,
+      nonew:true,
+      regexp:true,
+      undef:true,
+      strict:true,
+      trailing:true,
+      node:true
     };
   }
-})();
+}());
