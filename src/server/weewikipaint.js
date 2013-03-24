9ec3fa5a1f80a6bdee2d95bb588967c78dbb29c8
dan <dan@generalthings.com>
2013-03-24 10:25:02 -0700

    Factor out content directory

--- src/server/weewikipaint.js
+++ src/server/weewikipaint.js
@@ -1,9 +1,10 @@
 (function(){
   "use strict";
 
+  var CONTENT_DIR = "src/server/content";
   var server = require("./server.js");
   var port = process.argv[2];
-  server.start("src/server/content/home.html", "src/server/content/404.html", port, function(){
+  server.start(CONTENT_DIR + "/home.html", CONTENT_DIR + "/404.html", port, function(){
     console.log("Server started");
   });
 }());
