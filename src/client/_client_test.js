2c8bd9b82bd53f2648e42290e655a5d846acc3b4
dan <dan@generalthings.com>
2013-04-13 09:39:01 -0700

    Inserted an element into Testacular DOM

--- src/client/_client_test.js
+++ src/client/_client_test.js
@@ -1,15 +1,19 @@
 // Copyright (c) 2012 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
-/*global describe, it, expect*/
+/*global describe, it, expect, dump*/
 
 (function () {
-	"use strict";
-//var assert = cha.assert;
+  "use strict";
+  //var assert = cha.assert;
 
-	describe("Nothing", function () {
+  describe("Nothing", function () {
 
-		it("should run", function () {
-			expect("foo").to.equal("foo");
-		});
+    it("should run", function () {
+      var div = document.createElement("div");
+      div.setAttribute("id", "tdjs");
+      document.body.appendChild(div);
+      debugger
+      dump('breakpoint');
+    });
 
-	});
+  });
 }());
