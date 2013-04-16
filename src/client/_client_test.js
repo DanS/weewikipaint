df55b0b621ea386f027df98aef309d206c034d22
dan <dan@generalthings.com>
2013-04-16 08:46:26 -0700

    Distinguish between SVG and VMl Raphael by using Raphael.type flag

--- src/client/_client_test.js
+++ src/client/_client_test.js
@@ -1,5 +1,5 @@
 // Copyright (c) 2012 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
-/*global describe, it, expect, dump, $, wwp, afterEach*/
+/*global describe, it, expect, dump, $, wwp, afterEach Raphael*/
 
 (function() {
   "use strict";
@@ -21,21 +21,21 @@
 
       // verify it was initialized correctly
       var tagName = $(drawingDiv).children()[0].tagName.toLowerCase();
-      if (tagName === "svg") {
-        // We're in a browser that supports SVG
+      if (Raphael.type === 'svg'){
         expect(tagName).to.equal("svg");
       }
-      else {
-        // Check for IE 8
+      else if(Raphael.type === 'vml'){
         expect(tagName).to.equal("div");
-      }
+      } //else {
+        //expect().fail("Browser does not support Raphael");
+      //}
     });
 
     it("should have the same dimensions as its enclosing div", function() {
       drawingDiv = $("<div style='height: 200px; width: 400px'>hi</div>");
       $(document.body).append(drawingDiv);
 
-      var paper = wwp.initializeDrawingArea(drawingDiv[0]);
+        var paper = wwp.initializeDrawingArea(drawingDiv[0]);
       expect(paper.width).to.equal(400);
       expect(paper.height).to.equal(200);
     });
