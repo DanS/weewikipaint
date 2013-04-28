e7064b07f9e351b2032184eae826e03815c0e990
dan <dan@generalthings.com>
2013-04-28 15:10:34 -0700

    Test work on all browsers even when paths go from right to left

--- src/client/_client_test.js
+++ src/client/_client_test.js
@@ -1,10 +1,10 @@
 // Copyright (c) 2012 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
 /*global describe, it, expect, dump, jQuery, $, wwp, afterEach beforeEach Raphael*/
 
-(function() {
+(function () {
   "use strict";
 
-  describe("Drawing area", function() {
+  describe("Drawing area", function () {
 
     var drawingArea;
     var paper;
@@ -12,44 +12,47 @@
     //beforeEach(function(){
     //});
 
-    afterEach(function() {
+    afterEach(function () {
       drawingArea.remove();
     });
 
-		it("should have the same dimensions as its enclosing div", function() {
-			drawingArea = $("<div style='height: 300px; width: 600px'>hi</div>");
-			$(document.body).append(drawingArea);
-			paper = wwp.initializeDrawingArea(drawingArea[0]);
+    it("should have the same dimensions as its enclosing div", function () {
+      drawingArea = $("<div style='height: 300px; width: 600px'>hi</div>");
+      $(document.body).append(drawingArea);
+      paper = wwp.initializeDrawingArea(drawingArea[0]);
 
-			expect(paper.height).to.equal(300);
-			expect(paper.width).to.equal(600);
-		});
+      expect(paper.height).to.equal(300);
+      expect(paper.width).to.equal(600);
+    });
 
-		it("should draw a line", function() {
-			drawingArea = $("<div style='height: 300px; width: 600px'>hi</div>");
-			$(document.body).append(drawingArea);
-			paper = wwp.initializeDrawingArea(drawingArea[0]);
+    it("should draw a line", function () {
+      drawingArea = $("<div style='height: 300px; width: 600px'>hi</div>");
+      $(document.body).append(drawingArea);
+      paper = wwp.initializeDrawingArea(drawingArea[0]);
 
-			wwp.drawLine(20, 30, 30, 300);
-			expect(paperPaths(paper)).to.eql([ [20, 30, 30, 300] ]);
-		});
+      wwp.drawLine(20, 30, 30, 300);
+      expect(paperPaths(paper)).to.eql([
+        [20, 30, 30, 300]
+      ]);
+    });
 
-    it("draws line segments in response to clicks", function() {
+    it("draws line segments in response to clicks", function () {
       drawingArea = $("<div style='height: 200px; width: 400px'>hi</div>");
       $(document.body).append(drawingArea);
       paper = wwp.initializeDrawingArea(drawingArea[0]);
 
       clickMouse(20, 30);
       clickMouse(50, 60);
-//      clickMouse(40, 20);
+      clickMouse(40, 20);
 
-      expect(paperPaths(paper)).to.eql([[20, 30, 50, 60]]); //, [50, 60, 40, 20 ]]);
+      expect(paperPaths(paper)).to.eql([
+        [20, 30, 50, 60], [50, 60, 40, 20 ]]);
     });
 
-    function paperPaths(paper){
+    function paperPaths(paper) {
       var box;
       var result = [];
-      for(var i = 0; i < drawingElements(paper).length; i++){
+      for (var i = 0; i < drawingElements(paper).length; i++) {
         box = pathFor(drawingElements(paper)[i]);
         result.push([ box.x, box.y, box.x2, box.y2]);
       }
@@ -57,28 +60,28 @@
     }
 
     //it("considers border when calculating mouse target", function(){
-      //drawingArea = $("<div style='height: 200px; width: 400px borderW-width: 13px'>hi</div>");
-      //$(document.body).append(drawingArea);
-      //paper = wwp.initializeDrawingArea(drawingArea[0]);
+    //drawingArea = $("<div style='height: 200px; width: 400px borderW-width: 13px'>hi</div>");
+    //$(document.body).append(drawingArea);
+    //paper = wwp.initializeDrawingArea(drawingArea[0]);
 
-      //var eventData = new jQuery.Event();
-      //eventData.pageX = 20;
-      //eventData.pageY = 30;
-      //eventData.type = 'click';
+    //var eventData = new jQuery.Event();
+    //eventData.pageX = 20;
+    //eventData.pageY = 30;
+    //eventData.type = 'click';
 
-      //drawingArea.trigger(eventData);
+    //drawingArea.trigger(eventData);
 
-      //var topLeftOfDrawingArea = drawingArea.offset();
-      //var borderWidth = 13;
-      //var expectedX = 20 - topLeftOfDrawingArea.left - borderWidth;
-      //var expectedY = 30 - topLeftOfDrawingArea.top - borderWidth;
+    //var topLeftOfDrawingArea = drawingArea.offset();
+    //var borderWidth = 13;
+    //var expectedX = 20 - topLeftOfDrawingArea.left - borderWidth;
+    //var expectedY = 30 - topLeftOfDrawingArea.top - borderWidth;
 
-      //var elements = drawingElements(paper);
-      //expect(elements.length).to.equal(1);
-      //expect(pathFor(elements[0])).to.equal("M0,0L" + expectedX + ',' + expectedY);
+    //var elements = drawingElements(paper);
+    //expect(elements.length).to.equal(1);
+    //expect(pathFor(elements[0])).to.equal("M0,0L" + expectedX + ',' + expectedY);
     //});
 
-   function clickMouse(relativeX, relativeY) {
+    function clickMouse(relativeX, relativeY) {
       var topLeftOfDrawingArea = drawingArea.offset();
       var pageX = relativeX + topLeftOfDrawingArea.left;
       var pageY = relativeY + topLeftOfDrawingArea.top;
@@ -99,40 +102,50 @@
     }
 
     function pathFor(element) {
-//      var box = element.getBBox();
-//      return 'M' + box.x + ',' + box.y + 'L' + box.x2 + ',' + box.y2;
-
-      if(Raphael.vml) return vmlPathfor(element);
-      else if(Raphael.svg) return svgPathfor(element);
+      if (Raphael.vml) return vmlPathfor(element);
+      else if (Raphael.svg) return svgPathfor(element);
       else throw new Error("Unknown Raphael type");
     }
 
-    function svgPathfor(element){
+    function svgPathfor(element) {
       var path = element.node.attributes.d.value;
-      if(path.indexOf(",") !== -1){
-        return path;
-      }else{
+      if (path.indexOf(",") !== -1) {
+        var modernPathRegex = /M(\d+),(\d+)L(\d+),(\d+)/;
+        var modern = path.match(modernPathRegex);
+        return{
+          x: modern[1],
+          y: modern[2],
+          x2: modern[3],
+          y2: modern[4]
+        };
+      } else {
         var ie9PathRegex = /M (\d+) (\d+) L (\d+) (\d+)/;
         var ie9 = path.match(ie9PathRegex);
-        return "M" + ie9[1] + "," + ie9[2] + "L" + ie9[3] + "," + ie9[4];
+        return{
+          x: ie9[1],
+          y: ie9[2],
+          x2: ie9[3],
+          y2: ie9[4]
+        };
       }
+      return path;
     }
 
-    function vmlPathfor(element){
-       var VML_MAGIC_NUMBER = 21600;
-        var path = element.node.path.value;
-        var ie8PathRegex = /m(\d+),(\d+) l(\d+),(\d+) e/;
-        var ie8 = path.match(ie8PathRegex);
-        var startX = ie8[1] / VML_MAGIC_NUMBER;
-        var startY = ie8[2] / VML_MAGIC_NUMBER;
-        var endX = ie8[3] / VML_MAGIC_NUMBER;
-        var endY = ie8[4] / VML_MAGIC_NUMBER;
-        return{
-          x: startX,
-          y: startY,
-          x2: endX,
-          y2: endY
-       };
+    function vmlPathfor(element) {
+      var VML_MAGIC_NUMBER = 21600;
+      var path = element.node.path.value;
+      var ie8PathRegex = /m(\d+),(\d+) l(\d+),(\d+) e/;
+      var ie8 = path.match(ie8PathRegex);
+      var startX = ie8[1] / VML_MAGIC_NUMBER;
+      var startY = ie8[2] / VML_MAGIC_NUMBER;
+      var endX = ie8[3] / VML_MAGIC_NUMBER;
+      var endY = ie8[4] / VML_MAGIC_NUMBER;
+      return{
+        x: startX,
+        y: startY,
+        x2: endX,
+        y2: endY
+      };
     }
 
   });
