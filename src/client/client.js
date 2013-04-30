d54a979c17d7948b66baf9251bb9d1f988295c85
dan <dan@generalthings.com>
2013-04-30 08:31:22 -0700

    Factored out relative offset calculation

--- src/client/client.js
+++ src/client/client.js
@@ -8,18 +8,18 @@ wwp = {};
 
   var paper;
 
+
   wwp.initializeDrawingArea = function(drawingAreaElement) {
-    var startX = null;
-    var startY = null;
+    var start = null;
     var isDragging = false;
 
     paper = new Raphael(drawingAreaElement);
 
     $(document).mousedown(function(event){
       isDragging = true;
+      start = relativeOffset(drawingArea, event.pageX, event.pageY);
+
       var pageOffset = drawingArea.offset();
-      startX = event.pageX - pageOffset.left;
-      startY = event.pageY - pageOffset.top;
     });
 
     $(document).mouseup(function(event){
@@ -28,13 +28,10 @@ wwp = {};
 
     var drawingArea = $(drawingAreaElement);
     drawingArea.mousemove(function(event){
-      var pageOffset = drawingArea.offset();
-      var endX = event.pageX - pageOffset.left;
-      var endY = event.pageY - pageOffset.top;
-      
-      if(startX !== null && isDragging) wwp.drawLine(startX, startY, endX,  endY);
-      startX = endX;
-      startY = endY;
+      var end = relativeOffset(drawingArea, event.pageX, event.pageY);
+
+      if(start !== null && isDragging) wwp.drawLine(start.x, start.y, end.x,  end.y);
+      start = end;
     });
 
     //var prevX = null;
@@ -62,6 +59,15 @@ wwp = {};
     //});
     return paper;
   };
+
+  function relativeOffset(element, absoluteX, absoluteY){
+    var pageOffset = element.offset();
+
+    return {
+      x: absoluteX - pageOffset.left,
+      y: absoluteY - pageOffset.top
+    };
+  }
   
   wwp.drawLine = function(startX, startY,endX, endY){
     paper.path("M" + startX + "," + startY + "L" +  endX + "," + endY);
