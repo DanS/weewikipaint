d3e2f6ef5a10bd56e566d67d964b383517e95216
dan <dan@generalthings.com>
2013-04-30 08:41:59 -0700

    Made drag state handling a bit more compact

--- src/client/client.js
+++ src/client/client.js
@@ -3,64 +3,39 @@
 
 wwp = {};
 
-(function() {
+(function () {
   "use strict";
 
   var paper;
 
-
-  wwp.initializeDrawingArea = function(drawingAreaElement) {
+  wwp.initializeDrawingArea = function (drawingAreaElement) {
     var start = null;
-    var isDragging = false;
 
     paper = new Raphael(drawingAreaElement);
 
-    $(document).mousedown(function(event){
-      isDragging = true;
+    $(document).mousedown(function (event) {
       start = relativeOffset(drawingArea, event.pageX, event.pageY);
 
       var pageOffset = drawingArea.offset();
     });
 
-    $(document).mouseup(function(event){
-      isDragging = false;
+    $(document).mouseup(function (event) {
+      start = null;
     });
 
     var drawingArea = $(drawingAreaElement);
-    drawingArea.mousemove(function(event){
+    drawingArea.mousemove(function (event) {
       var end = relativeOffset(drawingArea, event.pageX, event.pageY);
 
-      if(start !== null && isDragging) wwp.drawLine(start.x, start.y, end.x,  end.y);
+      if (start === null) return;
+      wwp.drawLine(start.x, start.y, end.x, end.y);
       start = end;
     });
 
-    //var prevX = null;
-    //var prevY = null;
-    //var jqArea = $(drawingAreaElement);
-    //var isDragging = false;
-
-    
-    //jqArea.mousedown(function(event){
-      //isDragging = true;
-    //});
-
-    //jqArea.mouseup(function(event){
-      //isDragging = false;
-    //});
-
-    //jqArea.mousemove(function(event){
-      //var divPageX = $(jqArea).offset().left;
-      //var divPageY = $(jqArea).offset().top;
-      //var relativeX = event.pageX - divPageX;
-      //var relativeY = event.pageY - divPageY;
-      //if(prevX !== null && isDragging) wwp.drawLine(prevX, prevY, relativeX, relativeY);
-      //prevX = relativeX;
-      //prevY = relativeY;
-    //});
     return paper;
   };
 
-  function relativeOffset(element, absoluteX, absoluteY){
+  function relativeOffset(element, absoluteX, absoluteY) {
     var pageOffset = element.offset();
 
     return {
@@ -68,9 +43,9 @@ wwp = {};
       y: absoluteY - pageOffset.top
     };
   }
-  
-  wwp.drawLine = function(startX, startY,endX, endY){
-    paper.path("M" + startX + "," + startY + "L" +  endX + "," + endY);
+
+  wwp.drawLine = function (startX, startY, endX, endY) {
+    paper.path("M" + startX + "," + startY + "L" + endX + "," + endY);
   };
 
 }());
